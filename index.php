43e43dc1255d873a77a3c4508120784679170bee
Daryya24 <daryq24@gmail.com>
2022-02-11 19:02:34 +0200

    Task 2

--- index.php
+++ index.php
@@ -12,3 +12,16 @@ $order .= " Lab2 php ";
 //// Don't change the line below
 echo "<br>Your order is:$very_bad_unclear_name.";
 
+
+//Task 2
+$x = 5;
+echo $x . "<br>";
+$y = 10;
+echo $y;
+$z = 15.5;
+echo (11+1) . "<br>";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo ($last_month - $this_month);
+
