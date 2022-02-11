004f016d958b136fa9eaf940d9bd82cf79da8c03
Daryya24 <daryq24@gmail.com>
2022-02-11 18:41:01 +0200

    Task 1

--- index.php
+++ index.php
@@ -1,2 +1,13 @@
 <?php
 
+//Task 1
+
+/* Imagine a lot of code here */
+$very_bad_unclear_name="15 chicken wings";
+
+// Write your code here:
+
+
+//// Don't change the line below
+echo "<br>Your order is:$very_bad_unclear_name.";
+
